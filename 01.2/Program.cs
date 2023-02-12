0ffcdf6b52121b6a9e455a3cda733a4679e580b6
Georgy <x.ad@yandex.ru>
2023-02-12 15:53:48 +0300

    max (22 3 9)

---
+++ 01.2/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+int[] numbers = new int[3]{22, 3, 9}; 
+int max = numbers[0]; 
+for (int index = 0; index < numbers.Length; index++) 
+{ 
+ if (numbers[index] > max) max = numbers[index]; 
+} 
+Console.WriteLine("Максимальное число = " +max);
