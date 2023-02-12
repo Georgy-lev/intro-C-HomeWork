85537f63c4d881038b3c7dd619fd447406120ae4
Georgy <x.ad@yandex.ru>
2023-02-12 16:34:13 +0300

    Выводит все четные числа до N

---
+++ 3/Program.cs
@@ -0,0 +1,5 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введие число");
+int number = int.Parse(Console.ReadLine() !);
+for (int i = 0; i < number; i+=2)
+    Console.WriteLine(i);
