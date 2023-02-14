4365483859e3f513f610519cf6bc5e677249c688
Georgy <x.ad@yandex.ru>
2023-02-14 12:03:39 +0300

    5

---
+++ Семинар 2C#/5/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, 
+//является ли одно квадратом другого
+Console.WriteLine("Введите число");
+int number1 = int.Parse(Console.ReadLine() !);
+int number2 = int.Parse(Console.ReadLine() !);
+
+int square = number1*number1;
+int square2 = number2*number2;
+
+
+if ( square == number2||square2 == number1)
+    Console.WriteLine("Число являеся квадратом второго");
+else
+{
+    Console.WriteLine("Не является");
+}
