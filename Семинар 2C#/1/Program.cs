f195fd6ead52442ac7f39bb5ecfa4615d337ab68
Georgy <x.ad@yandex.ru>
2023-02-14 10:57:11 +0300

    2

--- Семинар 2C#/1/Program.cs
+++ Семинар 2C#/1/Program.cs
@@ -3,7 +3,7 @@ Console.Clear();
 
 int num = new Random().Next(10,100);//num = 25
 int a1 = num / 10; // a1 = 25 / 10 = 2
-int a2 = num %10 // a2 = 25 % 10 = 5
+int a2 = num %10; // a2 = 25 % 10 = 5
 int max = a1;
 if (max < a2)
     max = a2;
--- Семинар 2C#/2/Program.cs
+++ Семинар 2C#/2/Program.cs
@@ -1,10 +1,9 @@
 ﻿// Напишите программу, которая выводит случайное число из отрезка [10,99]
 Console.Clear();
 
-int num = new Random().Next(10,100);//num = 25
-int a1 = num / 10; // a1 = 25 / 10 = 2
-int a2 = num %10 // a2 = 25 % 10 = 5
-int max = a1;
-if (max < a2)
-    max = a2;
-Console.WriteLine($"Максимальная цифра числа {num}-> {max}");
+int num = new Random().Next(100,1000);//num = 25
+int a1 = num / 100; // a1 = 25 / 10 = 2
+int a3 = num % 10; // a2 = 25 % 10 = 5
+
+
+Console.WriteLine($"Максимальная цифра числа {num}-> {a1} {a3}");
