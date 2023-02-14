a29001c96f1dbb3e5032fb39ecf3af66b183ea9d
Georgy <x.ad@yandex.ru>
2023-02-14 11:36:30 +0300

    4

---
+++ Семинар 2C#/4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напиите программу, которая принимает на вход число и проверяет, критно
+// ли оно одновременно 7 и 23.
+
+int number = int.Parse(Console.ReadLine() !);
+
+int number1 = 7;
+int number2 = 23;
+
+if ( number % number1 == 0 || number % number2 == 0)
+    Console.WriteLine("Число кратно 7 и 23");
+else
+{
+    Console.WriteLine("Число некратно");
+}
+
