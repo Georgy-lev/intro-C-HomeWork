2dbf69d601ec20a724bc489ec21fd312405cd3fe
Georgy <x.ad@yandex.ru>
2023-02-17 11:33:25 +0300

    3

---
+++ Семинар 3C#/1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Напишите номер четверти ");
+int number= int.Parse(Console.ReadLine()!);
+if (number == 1)
+{
+    Console.WriteLine("x>0  y>0");
+}
+else if (number == 2)
+{
+    Console.WriteLine("x<0  y>0");
+}
+else if (number == 3)
+{
+    Console.WriteLine("x<0  y<0");
+}
+else if (number == 4)
+{
+    Console.WriteLine1("x>0  y<0");
+}
+
---
+++ Семинар 3C#/3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+//  и выдаёт таблицу квадратов чисел от 1 до N.
+
+// 5 -> 1, 4, 9, 16, 25.
+
+// 2 -> 1,4
+
+Console.Clear();
+Console.Write("Введите число: ");
+double number = double.Parse(Console.ReadLine());
+
+for (double i = 1; i <= number; i++) 
+{
+  double result = Math.Pow(i, 2);
+  Console.WriteLine($"{number} -> {result}");
+}
