f3b500159545619f22ebff88e239893ae110ac2c
Georgy <x.ad@yandex.ru>
2023-02-20 13:22:47 +0300

    HW3 #19

---
+++ HomeWork3/19/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число 
+//  и проверяет, является ли оно полиндромом.
+
+//14212-> нет
+//12821-> да
+//23432-> да
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int a1 = number / 10000; 
+int a5 = number %10;
+int a2 = number / 1000 % 10;
+int a4 = number %100 / 10;
+
+if (a1==a5|| a2==a4)
+    Console.WriteLine("Число является полиндромом");
+else
+{
+    Console.WriteLine("Число не является полиндромом");
+}
--- Семинар 3C#/3/Program.cs
+++ Семинар 3C#/3/Program.cs
@@ -12,5 +12,5 @@ double number = double.Parse(Console.ReadLine());
 for (double i = 1; i <= number; i++) 
 {
   double result = Math.Pow(i, 2);
-  Console.WriteLine($"{number} -> {result}");
+  Console.WriteLine($"{i} -> {result}");
 }
