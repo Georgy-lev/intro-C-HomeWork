5f687e0083c9cc5a08ba58a94bc06cddd6979d1f
Georgy <x.ad@yandex.ru>
2023-02-21 10:59:54 +0300

    Семинир 3 № 3

---
+++ Семинар 4C#/2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите прорамму, которая принимает на вход число 
+// и выдает количество цифр в числе
+// 456 -> 3
+// 78 -> 2
+//89126 -> 5
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+int count = 0;
+
+while (n!=0)
+{
+    n=n/10;
+    count++;
+}
+Console.WriteLine($"Количество цифр числа равна {count}");
---
+++ Семинар 4C#/3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите прорамму, которая принимает на вход число N
+// и выдает произведение цифр в числе
+
+int Get_Factorial(int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++)
+    result*= i;
+    return result;
+}
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Факториал от 1 до {n} равна {Get_Factorial(n)}");
