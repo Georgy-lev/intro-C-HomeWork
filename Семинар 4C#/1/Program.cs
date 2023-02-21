9efc925e0ac5d0f8c86dc425fbcfe93905e399e6
Georgy <x.ad@yandex.ru>
2023-02-21 10:16:03 +0300

    Семинир 3 №1

---
+++ Семинар 4C#/1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите прграмму, которая принимает на вход число А
+// и выдает сумму чисел от 1 до А
+
+int GetSum(int n)
+{
+    int sum = 0;
+    for (int i = 1; i < n; i++)
+    sum+= i;
+    return sum;
+}
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма от 1 до {n} равна {GetSum(n)}");
