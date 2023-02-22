cd1e11188ebb6e52d9892284c40b7811b0a97e84
Georgy <x.ad@yandex.ru>
2023-02-22 13:18:39 +0300

    HW № 29

---
+++ HomeWork4/29/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая задает массив из восьми элементов и выводит их на экран
+
+int[] arr_a(int length_arr){
+    int[] arr = new int[length_arr];
+    for(int i = 0; i < arr.Length; i ++){
+    arr[i] =  new Random().Next(9);
+    Console.Write($"{arr[i]}, ");
+}
+    return arr;
+}
+Console.WriteLine("Введите число : ");
+int num = int.Parse(Console.ReadLine()!);
+int[] d = arr_a(num);
