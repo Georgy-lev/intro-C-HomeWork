fc03b97c05e41871092f05e77d86c2df3b8bf0b2
Georgy <x.ad@yandex.ru>
2023-02-22 13:09:41 +0300

    HW № 27

---
+++ HomeWork4/27/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает число и выдает сумму его чисел.
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+int sum = 0;
+while (n>0)
+{
+    sum=sum+n%10;
+    n=n/10;
+}
+
+Console.WriteLine($"Сумма цифр числа {n} равна {sum}");
--- Семинар 4C#/3/Program.cs
+++ Семинар 4C#/3/Program.cs
@@ -3,7 +3,7 @@
 
 int Get_Factorial(int n)
 {
-    int result = 1;
+    int result =1;
     for (int i = 1; i <= n; i++)
     result*= i;
     return result;
