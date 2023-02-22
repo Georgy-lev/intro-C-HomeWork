8e5b1de2501ab8057a509aa0123abd2eecabe357
Georgy <x.ad@yandex.ru>
2023-02-22 11:06:41 +0300

    HW № 25

---
+++ HomeWork4/25/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("A: ");
+double A = double.Parse(Console.ReadLine()!);
+Console.Write("B: ");
+double B = double.Parse(Console.ReadLine()!);
+
+double D = Math.Pow(A ,  B);
+Console.WriteLine($"{A} в степени {B} = {D:f2}");
