15f0ab69ceee16293c244b70bee317a6b807f54b
Georgy <x.ad@yandex.ru>
2023-03-02 17:02:05 +0300

    Семинар 5 №3

---
+++ Семинар 5С#/3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве
+int [] arr = new int[5];
+for (int i = 0; i< arr.Length; i++)
+    arr[i]= new Random().Next(10);
+    Console.WriteLine(String.Join(" ", arr));
+
+Console.WriteLine("Введие число: ");  
+int num= int.Parse(Console.ReadLine()!); 
+if (arr.Contains(num)){
+    Console.WriteLine($"Число {num} присутствует в массиве");
+}
+else
+{
+    Console.WriteLine($"Число {num} отсутствует в массиве");
+}
