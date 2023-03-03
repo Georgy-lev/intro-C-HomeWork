8e0467cdfca27e7f9c903cbf3dcc957568398fa6
Georgy <x.ad@yandex.ru>
2023-03-03 10:00:27 +0300

    hw 41

---
+++ HomeWork6/41/Program.cs
@@ -0,0 +1,29 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Clear();
+Console.Write("Введите элементы массива через пробел: ");
+string elements = Console.ReadLine();
+int[] baseArray = GetArrayFromString(elements);
+
+Console.WriteLine(FindCountElements(baseArray));
+
+
+int[] GetArrayFromString(string stringArray)
+{
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] res = new int[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        res[i] = int.Parse(nums[i]);
+    }
+    return res;
+}
+int FindCountElements(int[] array)
+{
+    int count = 0;
+   foreach (var item in array)
+    {
+        if (item>0) count++;
+    }
+    return count;
+}
