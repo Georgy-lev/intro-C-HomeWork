0d4ab254c9e700fbeae509b2745963bb38dca6f2
Georgy <x.ad@yandex.ru>
2023-03-10 11:41:15 +0300

    Семинар 9

---
+++ Семинар 9C#/1/Program.cs
@@ -0,0 +1,20 @@
+﻿// **Задача 65:**Задайте значения M и N.
+//  Напишите программу, которая выведет все
+//   натуральные числа в промежутке от M до N.
+
+// M = 1; N = 5. -> "1, 2, 3, 4, 5"
+// M = 4; N = 8. -> "4, 5, 6, 7, 8"
+
+
+Write("Ent M: "); int m = int.Parse(ReadLine()!);
+Write("Ent N: "); int n = int.Parse(ReadLine()!);
+string PrintNums(int m, int n){ 
+    if (m==n)
+    {
+        WriteLine(m);
+        return m.ToString();
+    }
+    string res = PrintNums(m, n-1)+" "+n.ToString();
+    WriteLine(res);
+    return res;
+}
---
+++ Семинар 9C#/2/Program.cs
@@ -0,0 +1,21 @@
+﻿/***Задача 67:**Напишите программу, 
+которая будет принимать на вход число и возвращать сумму его цифр.
+
+453 -> 12
+
+45 -> 9*/
+
+
+Write("Ent NUM: "); int num = int.Parse(ReadLine());
+int sum=0;
+int Num(int number){
+    if (number == 0)
+    {
+        return number;
+    }
+     sum += number%10;
+    number=number/10;
+    Num(number);
+    return sum;}
+
+WriteLine(Num(num));
